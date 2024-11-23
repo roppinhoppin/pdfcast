c325acd75b745c1e8837299ef4c73926d26a6c0b
roppinhoppin <roppinhoppin@gmail.com>
2024-11-23 23:45:01 +0900

    Update after running retrieve.py

--- update.sh
+++ update.sh
@@ -8,6 +8,13 @@ echo "Done retrieve.py"
 ssh -t popos 'cd pdfcast && ./popos.sh'
 echo "Done popos.sh"
 git pull
+# copy magic-pdf/{num}/auto/images to image/{num}/
+for num in $(ls magic-pdf); do
+  if [ -d "magic-pdf/$num/auto/images" ]; then
+    mkdir -p "images/$num"
+    cp -r "magic-pdf/$num/auto/images/" "images/$num/"
+  fi
+done
 python retrieve.py
 git add .
 git commit -m "Update after running popos.sh and retrieve.py"
