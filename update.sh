a5705ba6a5f95a93739f948e80902e7813d0b85b
roppinhoppin <roppinhoppin@gmail.com>
2024-11-23 23:20:36 +0900

    Update after running retrieve.py

--- update.sh
+++ update.sh
@@ -1,9 +1,11 @@
 #!/bin/zsh
+git pull
 python retrieve.py --update
 git add .
 git commit -m "Update after running retrieve.py"
 git push
 ssh popos 'cd pdfcast && ./popos.sh'
+git pull
 python retrieve.py --update
 git add .
 git commit -m "Update after running popos.sh and retrieve.py"
